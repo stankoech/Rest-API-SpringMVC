38511408745ce9ed5edf1c7d97ed93f37dc73f23
stankoech <stanleykoech75@gmail.com>
2019-08-05 17:19:52 +0300

    create new resource using POST, Implementing update and delete

--- src/main/java/io/javabrains/springbootstarter/topic/TopicService.java
+++ src/main/java/io/javabrains/springbootstarter/topic/TopicService.java
@@ -1,5 +1,6 @@
 package io.javabrains.springbootstarter.topic;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,11 +9,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class TopicService {
 	
-	private List<Topic> topics = Arrays.asList(
+	private List<Topic> topics = new ArrayList <> (Arrays.asList(
 	        new Topic("Java","23", "s3"),
 	        new Topic("R","23", "s3"),
 	        new Topic("Python","23", "s3")
-	        );
+	        ));
 	
 	public List<Topic> getAllTopics(){
 		return topics;
@@ -22,4 +23,21 @@ public class TopicService {
 		return topics.stream().filter(t ->t.getId().equals(id)).findFirst().get();
 	}
 
+	public void addTopic(Topic topic) {
+		topics.add(topic);
+	}
+
+	public void updateTopic(String id, Topic topic) {
+	for (int i =0; i<topics.size(); i++) {
+		Topic t = topics.get(i);
+		if (t.getId().equals(id)) {
+			topics.set(i, topic);
+			return;
+		}
+	}
+	}
+
+	public void deleteTopic(String id) {
+		topics.removeIf(t -> t.getId().equals(id));		
+	}
 }
